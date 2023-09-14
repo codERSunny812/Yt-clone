c221f814fe5941704701ffb3792715c1db484c45
sunny <sengersunny448@gmail.com>
2023-09-15 03:15:42 +0530

    utils folder

---
+++ src/utils/fetchFromApi.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BaseUrl = "https://youtube-v31.p.rapidapi.com/captions";   
+
+const options = {
+  params: {
+    part: 'snippet',
+    videoId: 'M7FIvfx5J10'
+  },
+  headers: {
+    // eslint-disable-next-line no-undef
+    'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const  fetchFromApi = async (url) =>{
+   const { data } =   await axios.get(`${BaseUrl}/${url}`, options );
+   return data;
+}
