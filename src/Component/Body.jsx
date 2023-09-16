d22c81330b342094f607fb206591d17444922134
sunny <sengersunny448@gmail.com>
2023-09-16 10:47:03 +0530

    body Component

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -1,16 +1,24 @@
 import { Box, Stack , Typography } from "@mui/material";
 import SideBar from "./SideBar";
 import Videos from "./Videos";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useEffect, useState } from "react";
 
 
 
 
 function Body (){
+
+    const [selectedState , setSelectedState] = useState("New");
+
+    useEffect(() => {
+     fetchFromApi(`search?part=snippet&q=${selectedState}`);
+    },  [setSelectedState] );
     return (
 <Stack sx={{flexDirection:{sm:"column", md:"row"}}} > 
 <Box sx={{height:{sx:'auto', md:'92vh'} , borderRight:'1px solid #3d3d3d' , px:{sx:0 , md:2}}} > 
 {/* added the side bar  */}
-<SideBar/>
+<SideBar selectedState = {selectedState} setSelectedState={setSelectedState} />
 
 <Typography className="copyright"  variant= "body2" sx={{mt:15 , color:'#fff'}}>
     CopyRight 2023 Sunny 
@@ -20,7 +28,7 @@ function Body (){
 
 <Box p={2}>
     <Typography variant="h4" fontWeight="bold" mb={2} sx={{color:"white"}} >
-    New  <span style={{color:'#F31503'}}> Videos</span>
+   {selectedState} <span style={{color:'#F31503'}}> Videos</span>
     </Typography>
     {/* Now Render the videos section  */}
     <Videos/>
